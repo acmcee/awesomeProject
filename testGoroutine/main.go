894820101c09d93441182e2b627a2517a9e1710f
wanghangwei <wanghangwei@youzan.com>
2020-05-16 11:55:19 +0800

    限制goroutine 的最大核心

--- testGoroutine/main.go
+++ testGoroutine/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"awesomeProject/testGoroutine/goroutine"
 	"time"
+	"runtime"
+	"fmt"
 )
 
 func main() {
+	fmt.Println("cpu num: ", runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
 	go goroutine.TestLoop()
 	go goroutine.TestLoop()
 	go goroutine.TestLoop()
