70b8fc776a3f2e049a96a9ffc6a35c56b2302f30
wanghangwei <wanghangwei@youzan.com>
2020-05-16 10:19:47 +0800

    添加panic 的说明，以及不同的panic 类型的recover 捕获

--- testPanic/main.go
+++ testPanic/main.go
@@ -1,17 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	receivePanic()
 }
 
 func receivePanic()  {
+
 	defer coverPanic()
-	panic("i am  panic")
+	//panic("i am  panic")
+	//panic(errors.New("我是个error 类型"))
+	panic(1)
 }
 
 func coverPanic()  {
 	message:=recover()
-	fmt.Println("panic message: ", message)
+	switch message.(type) {  // 根据类型去捕获异常
+	case string:
+		fmt.Println("string message: ", message)
+	case error:
+		fmt.Println("error message: ", message)
+	default:
+		fmt.Println("unknow message:", message)
+	}
 }
