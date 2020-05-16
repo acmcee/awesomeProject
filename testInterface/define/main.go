b4c660380c51040d0fc42b9a1870e4fcf8a502fc
wanghangwei <wanghangwei@youzan.com>
2020-05-16 11:16:15 +0800

    添加接口的定义

---
+++ testInterface/define/main.go
@@ -0,0 +1,32 @@
+package main
+
+type Behavior interface {
+	Run() string
+	Eat() string
+}
+
+type Dog struct {
+	ID int
+	Name string
+	Age int
+}
+
+func (d *Dog)Eat() string {  // 已经隐式实现了接口
+	return "eating"
+}
+
+func (d *Dog)Run()  string { 	// 已经隐式实现了接口
+	return "running"
+}
+
+
+func main() {
+
+	dog := make(Dog)
+	dog.Name = "旺财"
+	dog.ID = 1
+	dog.Age = 5
+	dog.Eat()
+	dog.Run()
+
+}
