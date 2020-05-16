37dd0c3793f1a48fc049290f1fc60a3df8f88d99
wanghangwei <wanghangwei@youzan.com>
2020-05-16 12:18:53 +0800

    添加chan来生产者和消费者通信

--- testGoroutine/goroutine/goroutine.go
+++ testGoroutine/goroutine/goroutine.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+
+var mChan = make(chan int, 5)
 func TestLoop()  {
 	for i:=0;i<11 ;i++  {
 
@@ -12,4 +14,20 @@ func TestLoop()  {
 		time.Sleep(time.Microsecond * 100)
 	}
 	fmt.Println()
+}
+
+func TestSend()  {
+	for i:=0;i<5 ;i++  {
+		mChan <- i
+		fmt.Println("我发送了: ", i)
+		time.Sleep(time.Second)
+
+	}
+}
+
+func TestReceive()  {
+	for i:=0;i<5 ;i++  {
+		num:= <- mChan
+		fmt.Println("我收到了: ", num)
+	}
 }
--- testGoroutine/main.go
+++ testGoroutine/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"awesomeProject/testGoroutine/goroutine"
 	"time"
-	"runtime"
-	"fmt"
 )
 
 func main() {
-	fmt.Println("cpu num: ", runtime.NumCPU())
+	/*fmt.Println("cpu num: ", runtime.NumCPU())
 	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
 	go goroutine.TestLoop()
 	go goroutine.TestLoop()
 	go goroutine.TestLoop()
-	time.Sleep(time.Second)
+	time.Sleep(time.Second)*/
+
+
+	go goroutine.TestSend()
+	go goroutine.TestReceive()
+	time.Sleep(time.Second* 5)
+
 }
