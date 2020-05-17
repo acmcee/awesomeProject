ba53ad5a63254cefd10c2fc04693ac1007b0f543
wanghangwei <wanghangwei@youzan.com>
2020-05-17 11:07:41 +0800

    添加基于http handlefunc 的测试

--- testHTTP/main.go
+++ testHTTP/main.go
@@ -27,9 +27,29 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, s.message)
 }
 
+func RootHTTPHandler(w http.ResponseWriter, r *http.Request) {
+	message := ""
+	message += "有傻逼来访问啦哈哈哈哈\n"
+	message += fmt.Sprintln("r.URL.Path:", r.URL.Path)
+	message += fmt.Sprintln("r.Method：", r.Method)
+	message += fmt.Sprintln("r.Host:", r.Host)
+	message += fmt.Sprintln("r.URL.Query():", r.URL.Query())
+	message += fmt.Sprintln("r.URL.RawQuery", r.URL.RawQuery)
+	message += fmt.Sprintln("r.Body:", r.Body)
+	message += fmt.Sprintln("r.RequestURI", r.RequestURI)
+	message += fmt.Sprintln("a:", r.URL.Query().Get("a"))
+	message += "\n"
+	message += "\n"
+	message += "\n"
+	message += fmt.Sprintln(r)
+	fmt.Fprintf(w, message)
+}
+
 func main() {
-	http.Handle("/", &Server{})
-	http.HandleFunc()
+	// 测试使用对应的结构体来作为handler
+	//http.Handle("/", &Server{})
+	// 测试使用func 来作为handler
+	http.HandleFunc("/", RootHTTPHandler)
 	err := http.ListenAndServe(":8100", nil)
 	if err != nil {
 		fmt.Println(err.Error())
