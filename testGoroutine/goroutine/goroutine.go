fed1e596a8f77e48816664f80cb2193b27d9b614
wanghangwei <wanghangwei@youzan.com>
2020-05-19 15:23:06 +0800

    测试协程多个chan 发送消息

--- testGoroutine/goroutine/goroutine.go
+++ testGoroutine/goroutine/goroutine.go
@@ -60,3 +60,42 @@ func TestWrite() {
 		WG.Done()
 	}
 }
+
+func TestMultiChan() {
+	messages1 := make(chan int, 10)
+	messages2 := make(chan int, 10)
+	messages3 := make(chan int, 10)
+	done := make(chan bool)
+
+	defer close(messages1)
+	defer close(messages2)
+	defer close(messages3)
+	// consumer
+	go func() {
+		ticker := time.NewTicker(1 * time.Millisecond * 100)
+		for _ = range ticker.C {
+			select {
+			case <-done:
+				fmt.Println("child process interrupt...")
+				return
+			case d := <-messages1:
+				fmt.Printf("get from message1: %d\n", d)
+			case d := <-messages2:
+				fmt.Printf("get from message2: %d\n", d)
+			case d := <-messages3:
+				fmt.Printf("get from message3: %d\n", d)
+			}
+		}
+	}()
+
+	// producer
+	for i := 0; i < 10; i++ {
+		messages1 <- i + 100
+		messages2 <- i + 200
+		messages3 <- i + 300
+	}
+	time.Sleep(5 * time.Second)
+	close(done)
+	time.Sleep(1 * time.Second)
+	fmt.Println("main process exit!")
+}
