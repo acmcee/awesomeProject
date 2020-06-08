64a3fd732a0c8f4e992ea719c26263e7802b140c
wanghangwei <wanghangwei@youzan.com>
2020-06-08 10:12:22 +0800

    指针和引用都可以修改实参的内容，区别点如下
    引用修改是可以影响达到实参
    引用如果重新赋值的话，是无法修改到实参的
    
    修改指针的值是可以影响的实参，可以针对指针修改成完全不同的值
    指针重新赋值是无法影响到实参的，其实指针也是值传递。

--- testBuiltinFunc/valueRef/valueRef.go
+++ testBuiltinFunc/valueRef/valueRef.go
@@ -2,20 +2,56 @@ package main
 
 import "fmt"
 
-func ChangeSlice(s []string) {
-	fmt.Println("from change slice len:", len(s), ", cap:", cap(s))
-	s[1] = "new slice 1"         //  虽然是引用类型，发生了拷贝，但是指向的底层数组是同一个。
-	s = append(s, "new slice 2") // 对s 的引用重新赋值，因此修改对实参不生效
-	fmt.Println("from change slice len:", len(s), ", cap:", cap(s))
+func ChangeSlice1(s []string) {
+	fmt.Println("from ChangeSlice1 len:", len(s), ", cap:", cap(s))
+	s[1] = "modifyChangeSlice1" //  虽然是引用类型，发生了拷贝，但是指向的底层数组是同一个。
+	fmt.Println("ChangeSlice1 point1:", &s)
+	s = append(s, "AddChangeSlice1 ") // 对s 的引用重新赋值，因此修改对实参不生效
+	fmt.Println("ChangeSlice1 point2:", &s)
 
+}
+
+func ChangeSlice2(s []string) {
+	fmt.Println("from ChangeSlice2 len:", len(s), ", cap:", cap(s))
+	s[1] = "modifyChangeSlice2" //  虽然是引用类型，发生了拷贝，但是指向的底层数组是同一个。
+	fmt.Println("ChangeSlice2 point1:", &s)
+	sp := &s
+	*sp = append(*sp, "AddChangeSlice2 ") // 对s 的引用重新赋值，因此修改对实参不生效
+	fmt.Println("ChangeSlice2 point2:", &s)
+}
+
+func ChangeSlice3(s *[]string) {
+	fmt.Println("from ChangeSlice3 len:", len(*s), ", cap:", cap(*s))
+	(*s)[1] = "modifyChangeSlice3"
+	fmt.Println("ChangeSlice3 point1:", s)
+	*s = append(*s, "AddChangeSlice3 ") // 对s 指针的实际内容进行修改
+	fmt.Println("ChangeSlice3 point2:", s)
+}
+
+func ChangeSlice4(s *[]string) {
+	fmt.Println("from ChangeSlice3 len:", len(*s), ", cap:", cap(*s))
+	s = new([]string)
+	fmt.Println("ChangeSlice4 point1:", s)
+	*s = append(*s, "AddChangeSlice4 ") // 对s 的引用重新赋值，因此修改对实参不生效
+	fmt.Println("ChangeSlice4 point2:", s)
 }
 
 func main() {
 
-	s := make([]string, 2)
-	s[0] = "aaaa"
-	fmt.Println("from main slice len:", len(s), ", cap:", cap(s))
-	ChangeSlice(s)
+	s1 := make([]string, 2)
+	s2 := make([]string, 2)
+	s3 := make([]string, 2)
+	s4 := make([]string, 2)
+	ChangeSlice1(s1)
+	ChangeSlice2(s2)
+	ChangeSlice3(&s3)
+	ChangeSlice4(&s4)
+	fmt.Println("main s1", s1) //main s1 [ modifyChangeSlice1]
 
-	fmt.Println(s)
+	fmt.Println("main s2", s2) // main s2 [ modifyChangeSlice2]
+
+	fmt.Println("main s3", s3) // main s3 [ modifyChangeSlice3 AddChangeSlice3 ]
+
+	fmt.Println("main s4", s4) // main s4 [ ]
+
 }
