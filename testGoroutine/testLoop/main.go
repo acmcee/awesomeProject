1b3361315d72a72129dfb8385bb3f0ab200468b2
wanghangwei <wanghangwei@youzan.com>
2020-06-14 20:28:56 +0800

    测试wait group 使用，更加优雅的退出

--- testGoroutine/testLoop/main.go
+++ testGoroutine/testLoop/main.go
@@ -3,30 +3,55 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
-func TestMakeCake(i int, doneCh chan<- int, cooktime time.Duration) {
+func MakeCake(i int, doneCh chan<- int) {
+	rand.Seed(time.Now().UnixNano())
+	cooktime := time.Second * time.Duration(rand.Int()%5)
 	fmt.Println(i, "号蛋糕要做", cooktime)
 	time.Sleep(cooktime)
 	fmt.Println(i, "号蛋糕做好了")
 	doneCh <- i
 }
 
-func main() {
+func TestMakeCake1() {
 	makeNum := 10
 	ch := make(chan int, 2)
-	rand.Seed(time.Now().UnixNano())
-
 	for i := 0; i < makeNum; i++ {
+		go MakeCake(i, ch)
+	}
+	for i := 0; i < makeNum; i++ {
+		fmt.Println("我知道", <-ch, "号蛋糕做好了")
+	}
+	fmt.Println("全部蛋糕做好了")
+}
 
-		go TestMakeCake(i, ch, time.Second*time.Duration(rand.Int()%5))
+func TestMakeCake2() {
+	makeNum := 10
+	ch := make(chan int, 2)
+	var wg sync.WaitGroup
+	for i := 0; i < makeNum; i++ {
+		wg.Add(1)
+		go func(i int) {
+			MakeCake(i, ch)
+			wg.Done()
+		}(i)
 	}
 
-	for i := 0; i < makeNum; i++ {
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
 
-		fmt.Println("我知道", <-ch, "号蛋糕做好了")
+	for i := range ch {
+		fmt.Println("我知道", i, "做好了")
 	}
 
 	fmt.Println("全部蛋糕做好了")
+}
+
+func main() {
+	TestMakeCake2()
 }
