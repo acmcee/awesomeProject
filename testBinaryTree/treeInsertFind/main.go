61b21e2d92d4aed48c433fa403db01fd418a4087
wanghangwei <wanghangwei@youzan.com>
2020-07-10 14:35:15 +0800

    测试二叉树的插入和查找

---
+++ testBinaryTree/treeInsertFind/main.go
@@ -0,0 +1,95 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+func (n *Node) Find(k int) *Node {
+	if n != nil {
+		if k > n.data {
+			return n.left.Find(k)
+		} else if k < n.data {
+			return n.right.Find(k)
+		} else {
+			return n
+		}
+	} else {
+		return nil
+	}
+}
+
+func (n *Node) Insert(k int) *Node {
+	if k > n.data {
+		if n.left != nil {
+			return n.left.Insert(k)
+		} else {
+			n.left = new(Node)
+			n.left.data = k
+		}
+	} else if k < n.data {
+		if n.right != nil {
+			return n.right.Insert(k)
+		} else {
+			n.right = new(Node)
+			n.right.data = k
+		}
+	} else {
+		return n
+	}
+	return nil
+}
+
+func (n *Node) PreOrder() {
+	if n == nil {
+		return
+	}
+	fmt.Printf("%d ", n.data)
+	n.left.PreOrder()
+	n.right.PreOrder()
+}
+
+func (n *Node) InOrder() {
+	if n == nil {
+		return
+	}
+	n.left.InOrder()
+	fmt.Printf("%d ", n.data)
+	n.right.InOrder()
+}
+
+func (n *Node) PostOrder() {
+	if n == nil {
+		return
+	}
+	n.left.PostOrder()
+	n.right.PostOrder()
+	fmt.Printf("%d ", n.data)
+}
+
+func main() {
+	Root := new(Node)
+	Root.data = 100
+	Root.Insert(100)
+	Root.Insert(50)
+	Root.Insert(150)
+	Root.Insert(30)
+	Root.Insert(70)
+	Root.Insert(110)
+	Root.Insert(170)
+
+	//fmt.Println(Root)
+
+	//fmt.Println(Root.Find(150))
+	fmt.Println("前序：")
+	Root.PreOrder()
+	fmt.Println()
+	fmt.Println("中序：")
+	Root.InOrder()
+	fmt.Println()
+	fmt.Println("后序：")
+	Root.PostOrder()
+}
--- testGoroutine/channel/main.go
+++ testGoroutine/channel/main.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func ArraySource(a ...int) <-chan int {
+func ArraySource() <-chan int {
 
-	ch := make(chan int) // 由于这里是创建了无缓冲的channel，因此必须有个用goroutine 去写数据，否则就是deadlock了
+	ch := make(chan int, 100) // 由于这里是创建了无缓冲的channel，因此必须有个用goroutine 去写数据，否则就是deadlock了
 	// 否则就是写的时候堵塞，后面的读将无法执行到，main 卡死
 	go func() {
-		for i := range a {
+		for i := 0; i < 10; i++ {
 			ch <- i
 		}
 		close(ch)
@@ -16,18 +19,19 @@ func ArraySource(a ...int) <-chan int {
 }
 
 func main() {
-	ch1 := ArraySource(1, 2, 3, 4, 5, 6)
-	ch2 := ArraySource(1, 2, 3, 4, 5, 6)
-	for i := range ch1 { // 如果chan close 的话，可以直接用range
-		fmt.Println("ch1", i)
-	}
+	ch2 := ArraySource()
 
 	for {
 		if d, ok := <-ch2; ok { // 也可以用ok 来判断，chan 是否被关闭掉
 			fmt.Println("ch2", d)
+			fmt.Println("len", len(ch2))
+			fmt.Println("cap", cap(ch2))
 		} else {
+			fmt.Println("ch closed")
 			break
 		}
+		time.Sleep(time.Second)
+
 	}
 
 }
